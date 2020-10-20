dbd69c89e2d2c6bbf240af61f03a74ec0a08e3c2
Nishant Tripathi <nishanttripathi1910@gmail.com>
2020-10-20 22:35:56 +0530

    added physics energy

--- src/data/physicsenergy.js
+++ src/data/physicsenergy.js
@@ -1,13 +1,85 @@
 export const PhysicsEnergy = {
     title: 'Physics MCQ - Energys',
-    questions: [{
-        id: 1,
-        text: 'Energy Has',
-        correctAnswer: 'Both Magnitude and Direction',
-        values: ['Only Mgnitude', 'Both Magnitude and Direction', 'Only Direction', 'Neither Magnitude nor Direction'],
-        type: 'radio',
-        direction: 'V',
-        store: '',
+    questions: [
+      {
+      id: '1',
+      text: 'Velocity Ratio is',
+      correctAnswer: 'Velocity of effort/ Velocity of load',
+      values: ['Velocity of effort/ Velocity of load','Velocity of load/ Velocity of effort','Output/ Input','Input/Output'],
+      type : 'radio',
+      direction: 'V'
+      },
+      {
+        id: '2',
+        text: 'Velocity Ratio is',
+        correctAnswer: 'Velocity of effort/ Velocity of load',
+        values: ['Velocity of effort/ Velocity of load','Velocity of load/ Velocity of effort','Output/ Input','Input/Output'],
+        type : 'radio',
+        direction: 'V'
+      },
+      {
+        id: '3',
+        text: 'Mark the correct one',
+        correctAnswer: 'MA = Effort Arm/ Load Arm',
+        values: ['MA = Effort Arm/ Load Arm','Load/ Load Arm = Effort/ Effort Arm','Load X Effort = Load Arm X Effort Arm','Load X Effort Arm = Effort X Load Arm'],
+        type : 'radio',
+        direction: 'V'
+      },
+      {
+        id: '4',
+        text: 'Mechnical advantage of single moveable pully is ',
+        correctAnswer: '2',
+        values: ['1','1/2','2','3'],
+        type : 'radio',
+        direction: 'V'
+      },
+      {
+        id: '5',
+        text: 'Mechnical advantage of single fixed pully is ',
+        correctAnswer: '1',
+        values: ['1','1/2','2','3'],
+        type : 'radio',
+        direction: 'V'
+      },
+      {
+        id: '6',
+        text: 'Mechnical advantage of an inclined plane is ',
+        correctAnswer: 'Ratio of the length of inclined plane to the vertical rise',
+        values: ['Ratio of the length of inclined plane to the vertical rise','Ratio of the vertical rise to the length of inclined plane','h/l','Ration of distance covered to the ration of effort'],
+        type : 'radio',
+        direction: 'V'
+      },
+      {
+        id: '7',
+        text: 'What will be the mechnical advantage of a simple pully if a force of 25 kg is able to raise of load of 100 kg',
+        correctAnswer: '4',
+        values: ['4','8','2','100'],
+        type : 'radio',
+        direction: 'V'
+      },
+      {
+        id: '8',
+        text: 'The useful work done by a machine or the energy imparted by a machine is called its',
+        correctAnswer: 'output',
+        values: ['Input','Output','Mechnical Advantage','Velocity Ration'],
+        type : 'radio',
+        direction: 'V'
+      },
+      {
+        id: '9',
+        text: 'The external force applied to a machine is called',
+        correctAnswer: 'Effort',
+        values: ['Load','Effort','MA','VR'],
+        type : 'radio',
+        direction: 'V'
+      },
+      {
+        id: '10',
+        text: 'The load arm of a lever is 50cm long and its effort arm is 100 cm long. How much load can be lifted by using an effort of 10N',
+        correctAnswer: '20N',
+        values: ['10N','20N','30N','2N'],
+        type : 'radio',
+        direction: 'V'
       }
     ]
     };
